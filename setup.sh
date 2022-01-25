ac0adf7d28be60b8a70e322470ee7cb11d017d94
Nicholas Pleschko <n.pleschko@gmail.com>
2022-01-25 21:57:10 +0100

    Update to latest version of docker-compose

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ sh get-docker.sh
 popd
 
 echo "Installing Docker Compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Setting up proxy"
